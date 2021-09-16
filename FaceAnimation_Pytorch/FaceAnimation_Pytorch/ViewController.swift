71a94de519aaeedb62c0b9938e24eb8c6c7f042e
zhangerbing <zhangerbing@qilitech.ltd>
2021-09-16 11:35:06 +0800

    1. Initial project.

---
+++ FaceAnimation_Pytorch/FaceAnimation_Pytorch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceAnimation_Pytorch
+//
+//  Created by zhangerbing on 2021/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
